fe3197f4679ce6061388b31066471d4085064d24
Samuel Andersson <58047562+samswede@users.noreply.github.com>
2024-02-08 11:50:45 +0100

    sdfghjK

--- discover-coffee-stores/app/coffee-shops/[...name]/page.tsx
+++ discover-coffee-stores/app/coffee-shops/[...name]/page.tsx
@@ -3,12 +3,20 @@ import Link from 'next/link';
 export default function Page(props: {params: {name: string} }) {
 
   const { name } = props.params;
+
+  //console.log(name);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 
     bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90%">
       <h1>Discover Coffee Shops Near You</h1>
-      <h2>Name: {name}</h2>
-    
+      <ul>
+        {name.map((clip, idx) => (
+          <li key={idx}>
+            <h2>slug {idx}: {clip}</h2>
+          </li>
+        ))}
+      </ul>
+
       <ul>
         <li>
           <Link href="/coffee-shops">
